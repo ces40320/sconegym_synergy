a596680f12d3a182699661fbe7f799f2cecf6b75
Choi-Eunsik <98381473+ces40320@users.noreply.github.com>
2025-06-30 16:06:33 +0900

    Update init_v0.py
    sconewalk_h2190_uneven6 update

--- sconegym/init_v0.py
+++ sconegym/init_v0.py
@@ -19,7 +19,31 @@ register(id="sconewalk_h2190-v1",
              'rew_keys':{
                 "vel_coeff": 10,
                 "grf_coeff": 0,
-                "joint_limit_coeff": -0.1307,
+                # "joint_limit_coeff": -0.1307,
+                "joint_limit_coeff": -1,
+                "smooth_coeff": 0,
+                "nmuscle_coeff": 0,
+                "self_contact_coeff": 0.0,
+             }
+         },
+        )
+
+register(id="sconewalk_h2190_uneven6-v1",
+         entry_point="sconegym.gaitgym:GaitGym",
+         kwargs={
+             'model_file': curr_dir + '/data-v1/random_terrain_6.scone',
+             'obs_type': '3D',
+             'left_leg_idxs': [6, 7, 8, 9, 10, 11],
+             'right_leg_idxs': [12, 13, 14, 15, 16, 17],
+             'clip_actions': False,
+             'run': False,
+             'target_vel': 1.2,
+             'leg_switch': True,
+             'rew_keys':{
+                "vel_coeff": 10,
+                "grf_coeff": 0,
+                # "joint_limit_coeff": -0.1307,
+                "joint_limit_coeff": -1,
                 "smooth_coeff": 0,
                 "nmuscle_coeff": 0,
                 "self_contact_coeff": 0.0,
